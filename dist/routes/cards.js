7450e19e33b76ef1623bbce8c5b2a6e15bfc0604
BuTomer <bu.tomer@gmail.com>
2023-02-02 16:06:49 +0200

    cards

--- dist/routes/cards.js
+++ dist/routes/cards.js
@@ -1,21 +1,27 @@
 import { Router } from "express";
 import { Card } from "../db/models/card.js";
 const router = Router();
+import _ from "underscore";
+import { cardSchema } from "../validators/cards.js";
 //add cards to db:
 router.post("/", (req, res) => {
-    //TODO: add validations (next lecture)
-    const card = new Card({
-        name: req.body.name,
-        description: req.body.description,
-        address: req.body.address,
-        bizNumber: req.body.bizNumber,
-        image: req.body.image,
-        phone: req.body.phone,
-    });
+    const body = _.pick(req.body, "name", "description", "address", "bizNumber", "image", "phone");
+    //[{message:"", path:""}]=>[message:""]
+    const validationResult = cardSchema.validate(body);
+    const err = validationResult.error;
+    if (err) {
+        //bad request = 400 => validation errors
+        return res.status(400).json(err.details.map((o) => o.message));
+    }
+    const card = new Card(body);
     card
         .save()
         .then((saved) => {
-        res.json({ message: "Successfully saved your card" });
+        res.json({
+            message: "Successfully saved your card",
+            id: saved._id,
+            card: saved,
+        });
     })
         .catch((e) => {
         res.status(500).json({ message: `Error: ${e}` });
