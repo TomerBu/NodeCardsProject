a81fbabcff1fd3c525a9540293539bf8a598c2e6
BuTomer <bu.tomer@gmail.com>
2023-02-05 11:00:39 +0200

    role and user model and initdb with roles

--- dist/db/connect.js
+++ dist/db/connect.js
@@ -8,10 +8,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import mongoose from "mongoose";
+import dbConfig from "./config/db.config.js";
+import { Role } from "./models/role.js";
+const { HOST, DB, PORT, ROLES } = dbConfig;
 const connect = () => __awaiter(void 0, void 0, void 0, function* () {
     //mongoose 7 update:
     mongoose.set("strictQuery", false);
-    yield mongoose.connect("mongodb://127.0.0.1:27017/lec4db");
-    console.log("Succesfully connected to the database lec4db");
+    yield mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`);
+    console.log(`Succesfully connected to the database ${DB}`);
+    initDB();
+});
+const initDB = () => {
+    //create the User/Admin/Mod roles
+    //if Role collection is Empty:
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            ROLES.map((s) => new Role({ name: s })).forEach((role) => {
+                role.save((err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("added ", role.name, "to Roles collection");
+                    }
+                });
+            });
+        }
+    });
+};
+const initDB2 = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const count = yield Role.estimatedDocumentCount();
+        if (count === 0) {
+            const roles = ROLES.map((r) => new Role({ name: r }));
+            //dont use forEach with await
+            for (let role of roles) {
+                yield role.save();
+                console.log("added ", role.name, "to Roles collection");
+            }
+        }
+    }
+    catch (e) {
+        console.log("Failed with error: ", e);
+    }
 });
 export { connect };
