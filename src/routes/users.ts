d582ffef68ff060ab78541d435401d785f2de9fa
BuTomer <bu.tomer@gmail.com>
2023-02-07 13:02:37 +0200

    sending jwts after signin

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import authConfig from "../db/config/auth.config.js";
 import _ from "underscore";
 import { User } from "../db/models/user.js";
 import { validateSignUp } from "../middleware/verifySignupBody.js";
@@ -15,12 +17,12 @@ router.post("/signup", validateSignUp, userAlreadyExists, async (req, res) => {
 
   body.password = await bcrypt.hash(body.password, 12);
   const user = new User(body);
-  
+
   //before saving the user:
 
   try {
     //for each user -> save the role id of user
-    user.roles = [await (await Role.findOne({name: 'user'}))._id]
+    user.roles = [await (await Role.findOne({ name: "user" }))._id];
     await user.save();
     return res.json({ message: "user saved", id: user._id });
   } catch (e) {
@@ -31,14 +33,15 @@ router.post("/signup", validateSignUp, userAlreadyExists, async (req, res) => {
 router.post("/signin", validateSignIn, async (req, res) => {
   //email and password:
   try {
-    const user = await User.findOne({ email: req.body.email });
+    //SELECT * FROM user JOIN Roles ON ...
+    const user = await User.findOne({ email: req.body.email }).populate<{
+      roles: Array<typeof Role>;
+    }>("roles");
 
     if (!user) {
       return res.status(401).json({ message: "No Such User" });
     }
 
-    //123*12
-    //verify body.password matches user.password
     const isPasswordValid = await bcrypt.compare(
       req.body.password,
       user.password
@@ -48,8 +51,25 @@ router.post("/signin", validateSignIn, async (req, res) => {
       return res.status(401).json({ message: "Invalid Credentials" });
     }
 
-    return res.status(200).json({ message: "Sign in succesfull" });
-  } catch (e) {}
+    const token = jwt.sign({ id: user.id }, authConfig.secret, {
+      expiresIn: "30d",
+    });
+
+    const authorities = [];
+    for (let i = 0; i < user.roles.length; i++) {
+      authorities.push(`ROLE_` + user.roles[i].name.toUpperCase());
+    }
+
+    return res.status(200).json({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+      accessToken: token,
+    });
+  } catch (e) {
+    return res.status(500).json({ message: "Server error", error: e });
+  }
 });
 
 export { router as authRouter };
